c525945de7abdbe9b7db018ce5f0e549fcbc501c
Danny Gavin <dan.gavin49@gmail.com>
2026-08-02 06:48:16 -0700

    Update user model

--- models/user.py
+++ models/user.py
@@ -1,8 +1,7 @@
 from models import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
 
-class Users(UserMixin, db.Model):
+
+class Users(db.Model):
     __tablename__ = "users"
 
     # Primary Key
@@ -13,14 +12,22 @@ class Users(UserMixin, db.Model):
     username = db.Column(db.String(25), nullable=False)
     phone_number = db.Column(db.String(20))
     password = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        server_default=db.func.now()
+    )
+
     bio = db.Column(db.String(1000), nullable=True)
     location = db.Column(db.String(100))
-    # Account Status
-    is_active = db.Column(db.Boolean, default=True)
 
-    def get_id(self):
-        return str(self.id)
+    # Account Status
+    is_active = db.Column(
+        db.Boolean,
+        default=True
+    )
+
 
     def __repr__(self):
         return f"<User {self.email}>"
